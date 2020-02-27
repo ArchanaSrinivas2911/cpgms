f140d0ea3195dc8aef48a7a8d277121d85d9d719
ArchanaSrinivas2911 <61489626+ArchanaSrinivas2911@users.noreply.github.com>
2020-02-27 14:45:25 +0530

    Add files via upload

---
+++ prog4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter two numbers:\n");
+scanf("%d %d",&a,&b);
+if(a==b)
+printf("two numbers are equal\n");
+if(a>b)
+printf("a is greater than b\n");
+if(a<b)
+printf("a is less than b\n");
+}
---
+++ prog5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,average,total;
+printf("enter the five subjects\n");
+scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+total=a+b+c+d+e;
+average=(a+b+c+d+e)/2;
+printf("total of five subjects is = %d\n",total);
+printf("average of five subjects is  = %d\n",average);
+}
+
+
